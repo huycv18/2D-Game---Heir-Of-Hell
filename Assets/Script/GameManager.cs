cd9580f1bf14379503a75431cbb4d12d9183d542
hohuuthanh05 <hohuuthanh123123@gmail.com>
2026-02-22 16:25:19 +0700

    Call Boss = Energy

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentEnergy;
+
+    [SerializeField] private int energyThreshold = 3;
+    [SerializeField] private GameObject boss;
+    [SerializeField] private GameObject enemySpaner;
+
+    private bool bossCalled = false;
+
+    void Start()
+    {
+        boss.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void AddEnergy()
+    {
+        if (bossCalled)
+        {
+            return;
+        }
+
+        currentEnergy += 1;
+
+        if (currentEnergy == energyThreshold)
+        {
+            CallBoss();
+        }
+    }
+
+    private void CallBoss()
+    {
+        bossCalled = true;
+        boss.SetActive(true);
+        enemySpaner.SetActive(false);
+    }
+}
--- Assets/Script/PlayerCollision.cs
+++ Assets/Script/PlayerCollision.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    [SerializeField] private GameManager gameManager;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("EnemyBullet"))
         {
             PlayerController player = GetComponent<PlayerController>();
             player.TakeDamage(10f);
+        }
+        else if (collision.CompareTag("Usb"))
+        {
+            Debug.Log("Win Game Roi!!!");
+            Destroy(collision.gameObject);
+        }
+        else if (collision.CompareTag("Energy"))
+        {
+            gameManager.AddEnergy();
+            Destroy(collision.gameObject);
         }
     }
 }
