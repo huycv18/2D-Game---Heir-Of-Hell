d19ac0b861ebc63a2478bf3265df6835e7e982d9
hohuuthanh05 <hohuuthanh123123@gmail.com>
2026-02-22 18:12:10 +0700

    update sword

--- Assets/Script/AttackHitbox.cs
+++ Assets/Script/AttackHitbox.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class AttackHitbox : MonoBehaviour
 {
+    [SerializeField] private float damage = 40f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
-            Destroy(collision.gameObject);
+            Enemy enemy = collision.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
         }
     }
 }
